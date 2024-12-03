f51a73a2f6de1a80ffb7c21bdabb47229cf2b18e
RoyRyy <2524380603@qq.com>
2024-12-03 23:07:11 +0800

    Design GameJFrame menu bar.

--- game/src/com/itheima/ui/GameJFrame.java
+++ game/src/com/itheima/ui/GameJFrame.java
@@ -7,6 +7,47 @@ public class GameJFrame extends JFrame {
     //子类呢？也表示界面，窗体
     //规定：GameJFrame这个界面表示的就是游戏的主界面
     public GameJFrame() {
+        //初始化界面
+        initJFrame();
+
+        //初始化菜单
+        initJMenuBar();
+
+        //让界面显示出来，建议写在最后
+        this.setVisible(true);
+    }
+
+    private void initJMenuBar() {
+        //初始化菜单
+        //创建整个的菜单对象
+        JMenuBar menuBar = new JMenuBar();
+
+        //创建菜单上的两个选项的对象 (功能 关于我们)
+        JMenu functionJMenu = new JMenu("功能");
+        JMenu aboutJMenu = new JMenu("关于我们");
+
+        //创建选项下面的条目对象
+        JMenuItem replayItem = new JMenuItem("重新游戏");
+        JMenuItem reLoginItem = new JMenuItem("重新登录");
+        JMenuItem closeItem = new JMenuItem("关闭游戏");
+
+        JMenuItem accountItem= new JMenuItem("公众号");
+
+        //将每一个选项下面的条目添加到选项当中
+        functionJMenu.add(replayItem);
+        functionJMenu.add(reLoginItem);
+        functionJMenu.add(closeItem);
+
+        aboutJMenu.add(accountItem);
+
+        //将菜单里面的两个选项添加到菜单中
+        menuBar.add(functionJMenu);
+        menuBar.add(aboutJMenu);
+
+        this.setJMenuBar(menuBar);
+    }
+
+    private void initJFrame() {
         //设置界面的宽高
         this.setSize(603,680);
         //设置界面的标题
@@ -17,9 +58,6 @@ public class GameJFrame extends JFrame {
         this.setLocationRelativeTo(null);
         //设置关闭模式
         this.setDefaultCloseOperation(3);
-
-        //
-        this.setVisible(true);
     }
 
 }
