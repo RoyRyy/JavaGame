f0e333e40d4ba47b6922112a7321ca0b8887a49e
RoyRyy <2524380603@qq.com>
2024-12-03 17:36:43 +0800

    Add title of GameJFrame.

--- game/src/App.java
+++ game/src/App.java
@@ -1,3 +1,4 @@
+import com.itheima.ui.GameJFrame;
 import com.itheima.ui.LoginJFrame;
 
 public class App {
@@ -5,6 +6,8 @@ public class App {
         //表示程序的启动入口
 
         //如果我们想要开启一个界面，就创建谁的对象就可以了
-        new LoginJFrame();
+        //new LoginJFrame();
+
+        new GameJFrame();
     }
 }
--- game/src/com/itheima/ui/GameJFrame.java
+++ game/src/com/itheima/ui/GameJFrame.java
@@ -7,7 +7,13 @@ public class GameJFrame extends JFrame {
     //子类呢？也表示界面，窗体
     //规定：GameJFrame这个界面表示的就是游戏的主界面
     public GameJFrame() {
+        //设置界面的宽高
         this.setSize(603,680);
+        //设置界面的标题
+        this.setTitle("拼图单机版 v1.0");
+        //设置界面置顶
+        this.setAlwaysOnTop(true);
+        //
         this.setVisible(true);
     }
 
