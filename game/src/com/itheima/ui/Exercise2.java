c9dd94a420d5154bef0849802b6a41f85d500483
RoyRyy <2524380603@qq.com>
2024-12-08 16:59:24 +0800

    Exercise2

--- game/src/com/itheima/ui/Exercise2.java
+++ game/src/com/itheima/ui/Exercise2.java
@@ -2,13 +2,8 @@ package com.itheima.ui;
 
 import java.util.Random;
 
-public class Exercise1 {
-    /*
-    打乱一维数组中的数据
-    int[] tempArr={0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15};
-    并按照4个一组的方式添加到二维数组中。
-     */
-
+public class Exercise2 {
+    //解法2
     public static void main(String[] args) {
         int[] tempArr={0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15};
         Random r=new Random();
@@ -28,8 +23,12 @@ public class Exercise1 {
         int [][] data=new int[4][4];
 
         //5.给二维数组添加数据
-        for (int i = 0; i < tempArr.length; i++) {
-            data[i/4][i%4]=tempArr[i];
+       int index=0;
+        for (int i = 0; i < data.length; i++) {
+            for (int j = 0; j < data.length; j++) {
+                data[i][j]=tempArr[index];
+                index++;
+            }
         }
         for (int i = 0; i < data.length; i++) {
             for (int j = 0; j < data.length; j++) {
