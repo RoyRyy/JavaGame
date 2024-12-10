2eea27ebf40ec24a2aa52a20957e38a0ce302735
RoyRyy <2524380603@qq.com>
2024-12-10 20:22:04 +0800

    Add move picture action

--- game/src/com/itheima/ui/GameJFrame.java
+++ game/src/com/itheima/ui/GameJFrame.java
@@ -2,113 +2,96 @@ package com.itheima.ui;
 
 import javax.swing.*;
 import javax.swing.border.BevelBorder;
+import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import java.util.Random;
 
-public class GameJFrame extends JFrame {
-    //JFrame 界面，窗体
-    //子类呢？也表示界面，窗体
-    //规定：GameJFrame这个界面表示的就是游戏的主界面
+public class GameJFrame extends JFrame implements KeyListener, MouseListener {
+    int[][] data = new int[4][4];
+    int x = 0; // 空白块的行
+    int y = 0; // 空白块的列
 
-    //创建一个二维数组 目的：用来管理数据，加载图片时会根据二维数组中的数据进行加载
-    int [][] data=new int[4][4];
     public GameJFrame() {
-        //初始化界面
+        // 初始化界面
         initJFrame();
 
-        //初始化菜单
+        // 初始化菜单
         initJMenuBar();
 
-        //初始化数据（打乱）
+        // 初始化数据
         initData();
 
-        //初始化图片（根据打乱之后的结果去加载图片）
+        // 初始化图片
         initImage();
 
-        //让界面显示出来，建议写在最后
+        // 让界面显示出来
         this.setVisible(true);
+
+        // 给整个界面添加鼠标监听
+        this.addMouseListener(this);
     }
-    //初始化数据（打乱）
+
     private void initData() {
-        int[] tempArr={0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15};
-        Random r=new Random();
+        int[] tempArr = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15};
+        Random r = new Random();
         for (int i = 0; i < tempArr.length; i++) {
-            int index=r.nextInt(tempArr.length);
-            int temp=tempArr[i];
-            tempArr[i]=tempArr[index];
-            tempArr[index]=temp;
+            int index = r.nextInt(tempArr.length);
+            int temp = tempArr[i];
+            tempArr[i] = tempArr[index];
+            tempArr[index] = temp;
         }
 
-
-        //给二维数组添加数据
-        int index=0;
+        int index = 0;
         for (int i = 0; i < data.length; i++) {
-            for (int j = 0; j < data.length; j++) {
-                data[i][j]=tempArr[index];
+            for (int j = 0; j < data[i].length; j++) {
+                if (tempArr[index] == 0) {
+                    x = i;
+                    y = j; // 记录空白块的位置
+                }
+                data[i][j] = tempArr[index];
                 index++;
             }
         }
-
     }
 
-
-    //初始化图片
-    //添加图片时，需要按照二维数组中管理的数据添加图片
     private void initImage() {
-        //细节：
-        //先加载的图片在上方，后加载的图片塞在下面。
-        //路径分为两种：
-        //绝对路径：一定是从盘符开始
-        //相对路径：相对当前项目而言的
-
+        this.getContentPane().removeAll();
         for (int i = 0; i < 4; i++) {
             for (int j = 0; j < 4; j++) {
-                //获取当前要加载图片的序号
-                int num =data[i][j];
-                JLabel jLabel = new JLabel(new ImageIcon("image\\animal\\animal1\\"+num+".jpg"));
-                //指定图片位置
-                jLabel.setBounds(105*j+83, 105*i+134, 105, 105);
-                //给图片添加边框
-                //0:表示让图片凸起来
-                //1:凹下去
+                int num = data[i][j];
+                JLabel jLabel = new JLabel(new ImageIcon("image\\animal\\animal1\\" + num + ".jpg"));
+                jLabel.setBounds(105 * j + 83, 105 * i + 134, 105, 105);
                 jLabel.setBorder(new BevelBorder(1));
-                //把管理容器添加到界面中
                 this.getContentPane().add(jLabel);
-                //添加一次之后，number需要自增
-
             }
         }
-        //添加背景图片
-        ImageIcon icon=new ImageIcon("image\\background.png");
-        JLabel background=new JLabel(icon);
-        background.setBounds(40,40,508,560);
-        //把背景图片添加到界面当中
+
+        ImageIcon icon = new ImageIcon("image\\background.png");
+        JLabel background = new JLabel(icon);
+        background.setBounds(40, 40, 508, 560);
         this.getContentPane().add(background);
+
+        this.getContentPane().repaint();
     }
 
     private void initJMenuBar() {
-        //初始化菜单
-        //创建整个的菜单对象
         JMenuBar menuBar = new JMenuBar();
-
-        //创建菜单上的两个选项的对象 (功能 关于我们)
         JMenu functionJMenu = new JMenu("功能");
         JMenu aboutJMenu = new JMenu("关于我们");
 
-        //创建选项下面的条目对象
         JMenuItem replayItem = new JMenuItem("重新游戏");
         JMenuItem reLoginItem = new JMenuItem("重新登录");
         JMenuItem closeItem = new JMenuItem("关闭游戏");
+        JMenuItem accountItem = new JMenuItem("公众号");
 
-        JMenuItem accountItem= new JMenuItem("公众号");
-
-        //将每一个选项下面的条目添加到选项当中
         functionJMenu.add(replayItem);
         functionJMenu.add(reLoginItem);
         functionJMenu.add(closeItem);
-
         aboutJMenu.add(accountItem);
 
-        //将菜单里面的两个选项添加到菜单中
         menuBar.add(functionJMenu);
         menuBar.add(aboutJMenu);
 
@@ -116,19 +99,81 @@ public class GameJFrame extends JFrame {
     }
 
     private void initJFrame() {
-        //设置界面的宽高
-        this.setSize(603,680);
-        //设置界面的标题
+        this.setSize(603, 680);
         this.setTitle("坤坤爱拼图 v1.0");
-        //设置界面置顶
         this.setAlwaysOnTop(true);
-        //设置界面居中
         this.setLocationRelativeTo(null);
-        //设置关闭模式
-        this.setDefaultCloseOperation(3);
-
-        //取消默认的居中放置，只有取消了才会按照XY轴的形式添加组件
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setLayout(null);
+        this.addKeyListener(this);
     }
 
+    @Override
+    public void keyTyped(KeyEvent e) {}
+
+    @Override
+    public void keyPressed(KeyEvent e) {}
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        int code = e.getKeyCode();
+        if (code == 37 && y < 3) { // 左
+            data[x][y] = data[x][y + 1];
+            data[x][y + 1] = 0;
+            y++;
+            initImage();
+        } else if (code == 38 && x < 3) { // 上
+            data[x][y] = data[x + 1][y];
+            data[x + 1][y] = 0;
+            x++;
+            initImage();
+        } else if (code == 39 && y > 0) { // 右
+            data[x][y] = data[x][y - 1];
+            data[x][y - 1] = 0;
+            y--;
+            initImage();
+        } else if (code == 40 && x > 0) { // 下
+            data[x][y] = data[x - 1][y];
+            data[x - 1][y] = 0;
+            x--;
+            initImage();
+        }
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+        // 获取鼠标点击的位置
+        int clickX = e.getX();
+        int clickY = e.getY();
+
+        // 计算点击位置的行列
+        int row = (clickY - 134) / 105;
+        int col = (clickX - 83) / 105;
+
+        // 判断是否在有效范围内
+        if (row >= 0 && row < 4 && col >= 0 && col < 4) {
+            // 判断是否是空白块旁边
+            if ((row == x && Math.abs(col - y) == 1) || (col == y && Math.abs(row - x) == 1)) {
+                // 交换空白块和点击块
+                data[x][y] = data[row][col];
+                data[row][col] = 0;
+                x = row;
+                y = col;
+                initImage();
+            }
+        }
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {}
+
+    @Override
+    public void mouseReleased(MouseEvent e) {}
+
+    @Override
+    public void mouseEntered(MouseEvent e) {}
+
+    @Override
+    public void mouseExited(MouseEvent e) {}
 }
+
